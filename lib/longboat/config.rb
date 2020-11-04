0dc913384d118855a3ea24aed9c7294e6f2cf745
Nat Lasseter <nat.lasseter@york.ac.uk>
2020-11-04 10:58:26 +0000

    Align columns of options in config.rb

--- lib/longboat/config.rb
+++ lib/longboat/config.rb
@@ -5,15 +5,15 @@ module Longboat
     def self.parse!
       parser = Optimist::Parser.new do
         # Collection interval
-        opt :raid_every,     "Collection interval",               type: Integer, default: 60
+        opt :raid_every,     "Collection interval",               type: Integer,    default: 60
 
         # Job data
-        opt :raiders_path,   "Paths to search for raiders",       type: String,  default: "./lib/raiders",     multi: true
-        opt :metric_prefix,  "Prefix for metric names",           type: String,  default: "longboat_"
+        opt :raiders_path,   "Paths to search for raiders",       type: String,     default: "./lib/raiders",     multi: true
+        opt :metric_prefix,  "Prefix for metric names",           type: String,     default: "longboat_"
 
         # Sinatra server
-        opt :server_bind,    "Server listening address",          type: String,  default: "127.0.0.1:8564"
-        opt :server_path,    "Path to metrics",                   type: String,  default: "/metrics"
+        opt :server_bind,    "Server listening address",          type: String,     default: "127.0.0.1:8564"
+        opt :server_path,    "Path to metrics",                   type: String,     default: "/metrics"
 
         # Testing
         opt :test,           "Output metrics to stdout and quit", type: TrueClass,  default: false
